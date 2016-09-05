01716d64ff11992ab1ad496aff33e19e26d5ddea
Brian Nguyen <briannoogin@yahoo.com>
2016-09-05 12:59:01 -0500

    three layers works
    The sigmoid activation function does not work when it used as output
    layer. I had to use hyperbolic tanget in order to get it to work.

--- multilayerNetwork.m
+++ multilayerNetwork.m
@@ -11,7 +11,7 @@ trainingInputMatrix = trainNumeric(:,2:10);
 testInputMatrix = testNumeric(:,2:10);
 % Creates network with three layers
 net = feedforwardnet(10);
-net.numlayers = 3;
+net.numlayers = 4;
 % Tranpose data so it works with the toolbox
 trainTargetVector = trainTargetVector.';
 trainingInputMatrix = trainingInputMatrix.';
@@ -20,22 +20,27 @@ testInputMatrix = testInputMatrix.';
 % Name the layers
 net.layers{1}.name = 'Hidden Layer 1';
 net.layers{2}.name = 'Hidden Layer 2';
-net.layers{3}.name = 'Output';
+net.layers{3}.name = 'Hidden Layer 3';
+net.layers{4}.name = 'Output';
 % Designate the activation functions and number of neural units
 net.layers{1}.transferFcn = 'tansig';
 net.layers{2}.transferFcn = 'tansig';
 net.layers{3}.transferFcn = 'logsig';
-net.layers{1,1}.size = 50;
+net.layers{4}.transferFcn = 'tansig';
+net.layers{1,1}.size = 100;
 net.layers{2,1}.size = 25;
+net.layers{3,1}.size = 10;
 % Connects layers and bias units
-net.layerConnect = [0 0 0; 1 0 0; 0 1 0];
-net.biasConnect = [1;1;0];
-net.outputConnect = [0 0 1];
+net.layerConnect = [0 0 0 0; 1 0 0 0; 0 1 0 0;0 0 1 0];
+net.biasConnect = [1;1;1;1];
+net.outputConnect = [0 0 0 1];
 % Shape the network to the data
 net = configure(net,trainingInputMatrix,trainTargetVector);
-net.performParam.regularization = .4;
-% Train network 
+net.performParam.regularization = .5;
+% Change train settings and train network 
+net.trainParam.mu_max = 1.0000e+100;
 [net,record] = train(net,trainingInputMatrix,trainTargetVector);
+%net = adapt(net,trainingInputMatrix,trainTargetVector);
 % Calculating percent correct
 y = net(trainingInputMatrix);
 y = y > .51;
@@ -43,4 +48,6 @@ percentCorrect = find(y == trainTargetVector);
 percentCorrect = size(percentCorrect,2) / size(trainTargetVector,2);  
 % Test Performance
 testPerformance = perform(net,testInputMatrix,testTargetVector);
-view(net); 
+view(net);
+net
+sim(net);
