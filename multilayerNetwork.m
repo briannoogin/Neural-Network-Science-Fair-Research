a778f8cec9ce5b308ebd7adfe1cd259db4756dfa
Brian Nguyen <briannoogin@yahoo.com>
2016-09-08 00:47:41 -0500

    3 layers works
    got around 98% correct, the error between training and test were similar

--- multilayerNetwork.m
+++ multilayerNetwork.m
@@ -1,14 +1,14 @@
 % Reads Excel File
-trainingfileName = 'breastTraining.xlsx';
+trainingfileName = 'breastTrainData.xlsx';
 [trainNumeric,text,excel] = xlsread(trainingfileName);
-testfileName = 'breastTraining.xlsx';
+testfileName = 'breastTestData.xlsx';
 [testNumeric,testText,testExcel] = xlsread(testfileName);
 % Gets the data targets
 trainTargetVector = trainNumeric(:,1);
 testTargetVector = testNumeric(:,1);
 % Gets the data
-trainingInputMatrix = trainNumeric(:,2:10);
-testInputMatrix = testNumeric(:,2:10);
+trainingInputMatrix = trainNumeric(:,2:size(excel,2));
+testInputMatrix = testNumeric(:,2:size(excel,2));
 % Creates network with three layers
 net = feedforwardnet(10);
 net.numlayers = 4;
@@ -28,26 +28,28 @@ net.layers{2}.transferFcn = 'tansig';
 net.layers{3}.transferFcn = 'logsig';
 net.layers{4}.transferFcn = 'tansig';
 net.layers{1,1}.size = 100;
-net.layers{2,1}.size = 25;
-net.layers{3,1}.size = 10;
+net.layers{2,1}.size = 50;
+net.layers{3,1}.size = 25;
 % Connects layers and bias units
 net.layerConnect = [0 0 0 0; 1 0 0 0; 0 1 0 0;0 0 1 0];
 net.biasConnect = [1;1;1;1];
 net.outputConnect = [0 0 0 1];
 % Shape the network to the data
 net = configure(net,trainingInputMatrix,trainTargetVector);
-net.performParam.regularization = .5;
+net.performParam.regularization = .75;
+net.trainFcn = 'trainscg';
 % Change train settings and train network 
 net.trainParam.mu_max = 1.0000e+100;
-[net,record] = train(net,trainingInputMatrix,trainTargetVector);
+[net,record] = train(net,trainingInputMatrix,trainTargetVector,'useGPU','yes');
+trainPerformance = perform(net,trainingInputMatrix,trainTargetVector);
 %net = adapt(net,trainingInputMatrix,trainTargetVector);
 % Calculating percent correct
 y = net(trainingInputMatrix);
-y = y > .51;
+y = y > .50;
 percentCorrect = find(y == trainTargetVector);
 percentCorrect = size(percentCorrect,2) / size(trainTargetVector,2);  
 % Test Performance
 testPerformance = perform(net,testInputMatrix,testTargetVector);
 view(net);
 net
-sim(net);
+
