fa5adcd968fb144b3065b98ebd99af1f41c08b11
Brian Nguyen <briannoogin@yahoo.com>
2017-02-12 02:12:04 -0600

    fixed the folding
    I fixed the folding, but the actual training seems off. I have to
    convert the MSE to percent correct.

--- crossValidation.m
+++ crossValidation.m
@@ -1,10 +1,9 @@
-function[percentError] = crossValidation(net,foldNumber)
+function[percentError] = crossValidation(net,foldNumber,excelName)
 %% Reads Excel File
-trainingfileName = 'breastCancerFullDataSet';
+trainingfileName = excelName;
 [trainNumeric,text,excel] = xlsread(trainingfileName);
 folds = foldNumber;
-error = zeros(foldNumber,1);
-trainTargetVector = trainNumeric(:,1);
+error = zeros(folds,1);
 %% Randomly Sort Data
 % Generate random numbers from 1 to number of training examples
 randomPerms = randperm(size(trainNumeric,1)).';
@@ -13,29 +12,35 @@ trainNumeric = [randomPerms,trainNumeric];
 % Sort the matrix based on the random number
 trainNumeric = sortrows(trainNumeric);
 % Remove the random numbers in the first column
-trainNumeric = trainNumeric(:,2:size(trainNumeric,2));
+trainNumeric = trainNumeric(:,2:size(trainNumeric,2)).';
 
 %% Cross-Validation
 % Rounds fold size to the nearest integer so that all folds all the same
-foldSize = floor(size(trainNumeric,1) / folds);
+foldSize = floor(size(trainNumeric,2) / folds);
 startIndex = 1;
 endIndex = foldSize;
+% Check to see if folds are correct
+foldCheck = struct('TestFolds',0,'TrainFolds',0);
 for numberOfFoldsRan = 1:folds
-    testFold = trainNumeric(startIndex:endIndex,:);
+    testFold = trainNumeric(:,startIndex:endIndex);
     % Combines all the folds after the one test fold
     if startIndex == 1
-        trainFold = trainNumeric(endIndex + 1:size(trainNumeric,1),:);
+        trainFold = trainNumeric(:,endIndex + 1:size(trainNumeric,2));
     % Combines all the folds except the one test fold
     else
-        trainFold = cat(1,trainNumeric(1:startIndex,:),trainNumeric(endIndex:size(trainNumeric,1),:));
+        trainFold = cat(2,trainNumeric(:,1:startIndex-1),trainNumeric(:,endIndex+1:size(trainNumeric,1)));
     end
     startIndex = startIndex + foldSize;
     endIndex = endIndex + foldSize;
-    % Add the variables 
-    trainFold = cat(1,text,num2cell(trainFold));
-    testFold = cat(1,text,num2cell(testFold));
+    foldCheck(numberOfFoldsRan).TestFolds = testFold;
+    foldCheck(numberOfFoldsRan).TrainFolds = trainFold;
+    % Divide the folds into their data and their known output
+    trainFoldTargetVec = trainFold(1,:);
+    testFoldTargetVec = testFold(1,:);
+    trainFoldData = trainFold(2:size(trainFold,1),:);
+    testFoldData = testFold(2:size(testFold,1),:);
     % Train Network
-    [net,record] = train(net,trainNumeric,trainTargetVector,'useGPU','yes');
-    error(folds) = net(testFold);
+    [net,record] = train(net,trainFoldData,trainFoldTargetVec,'useGPU','yes');
+    error(numberOfFoldsRan) = perform(net,testFoldData,testFoldTargetVec);
 end
 percentError = mean(error);
